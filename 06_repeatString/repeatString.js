197501dce0f4de62b8042641e525006ef002fe63
MikeO <38439752+mikeOttenhoff@users.noreply.github.com>
2025-07-07 16:06:25 +0200

    First excersises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let step = 0; step < times; step++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
