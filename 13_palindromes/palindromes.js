e5cb836933f1f226891b93055a8793d12e9ec48f
MikeO <38439752+mikeOttenhoff@users.noreply.github.com>
2025-07-29 10:36:36 +0200

    Update palindromes.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const reverseWord = word.split("").reverse().join();
+  if (word === reverseWord) {
+    return console.log("This is a palindrome!");
+  } else {
+    return console.log("This is not a palindrome!");
+  }
 };
-
+palindromes("racecar"); // true
+palindromes("tacos"); // false
 // Do not edit below this line
 module.exports = palindromes;
