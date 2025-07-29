e0d2f06529fad9cc91db8dcb8c98ed57b08d7f21
MikeO <38439752+mikeOttenhoff@users.noreply.github.com>
2025-07-29 10:27:05 +0200

    changes

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  const arr = [[1, 2, 3, 4], 3, [1, 2, 3, 4], 1, 4, 3, [1, 2, 3, 4]];
+  const cleaned = arr
+    .filter(function (item) {
+      console.log(item);
+      return item !== 3;
+    })
+    .map(function (item) {
+      if (Array.isArray(item)) {
+        return item.filter(function (subItem) {
+          return subItem !== 3;
+        });
+      }
+      return item;
+    });
+  return cleaned;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (...theArgs) {
+  let total = 0;
+  for (const arg of theArgs) {
+    total += arg;
+  }
+  return total;
 };
 
 // Do not edit below this line
