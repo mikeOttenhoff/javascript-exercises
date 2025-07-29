ea7623495043b9ab983063c4a0d2ebe150d3c29a
MikeO <38439752+mikeOttenhoff@users.noreply.github.com>
2025-07-29 11:38:31 +0200

    Palindromes and Fibonaci changes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,12 +1,27 @@
-const palindromes = function (word) {
-  const reverseWord = word.split("").reverse().join();
-  if (word === reverseWord) {
-    return console.log("This is a palindrome!");
-  } else {
-    return console.log("This is not a palindrome!");
-  }
+const palindromes = function (string) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  // Convert to lowercase, split to array of individual characters, filter only valid characters, then rejoin as new string
+  const cleanedString = string
+    .toLowerCase()
+    .split("")
+    .filter(character => alphanumerical.includes(character))
+    .join("");
+
+  // Create a new reversed string for comparison
+  const reversedString = cleanedString.split("").reverse().join("");
+
+  // Return the outcome of the comparison which will either be true or false
+  return cleanedString === reversedString;
 };
-palindromes("racecar"); // true
-palindromes("tacos"); // false
+// console.log(palindromes("racecar")); // true
+// console.log(palindromes("racecar!")); // false
+// console.log(palindromes("Racecar!"));
+// console.log(palindromes("A car, a man, a maraca"));
+// console.log(palindromes("Animal loots foliated detail of stool lamina"));
+// console.log(palindromes("ZZZZ car, a man, a maracaz."));
+// console.log(palindromes("rac3e3car"));
+// console.log(palindromes("r3ace3car"));
+
 // Do not edit below this line
 module.exports = palindromes;
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+let sequenceTotal = [];
 
+const sequence = function (n) {
+  let i;
+  let fib = [0, 1];
+
+  for (i = 2; i <= n; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+  sequenceTotal = fib;
+};
+sequence(25);
+
+let fibonaci = function (number) {
+  return sequenceTotal.at(number);
 };
 
 // Do not edit below this line
